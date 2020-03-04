a43c4cb14fb684bf7ac3e9402ea465f94e2a79a2
amosshahar <amos.shahar@gmail.com>
2020-03-04 17:00:20 +0200

    travis yml file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          amos simple test
+          amos simple test ver3
         </p>
         <a
           className="App-link"
